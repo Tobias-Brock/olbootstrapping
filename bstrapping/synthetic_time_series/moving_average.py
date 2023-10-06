ee79105bdd59e88c2867c0d50ed047c5c874a9ad
Nico Palm <nicolaipalm@googlemail.com>
2023-10-06 15:29:42 +0200

    adapt benchmark notebook

--- bstrapping/synthetic_time_series/moving_average.py
+++ bstrapping/synthetic_time_series/moving_average.py
@@ -4,14 +4,15 @@ import numpy as np
 class MovingAverage:
     def __init__(self, mean: float, parameters: np.ndarray):
         self.mean = mean
-        self.parameters = np.append(parameters, 1)
+        self.parameters = np.concatenate(([1], parameters))
         self.samples = []
 
     def generate_samples(self, number_samples: int) -> np.ndarray:
         noise_terms = np.random.normal(loc=0, scale=1, size=number_samples + len(self.parameters))
+        q = len(self.parameters)
 
-        self.samples = np.array([self.mean + np.sum(
-            self.parameters * noise_terms[i:i + len(self.parameters)]) for i in range(number_samples)])
+        self.samples = np.array([self.mean + np.sum([self.parameters[j] * noise_terms[i - j] for j in range(q)])
+                                 for i in range(number_samples)])
 
         return self.samples
 
@@ -19,10 +20,9 @@ class MovingAverage:
     def asymptotic_variance(self) -> float:
         # limit var(1/n^1/2*sum X_i)
         q = len(self.parameters)
-        dummy = np.sum(
-            [np.sum(self.parameters[:q - i] * self.parameters[i:]) for i in range(1, len(self.parameters))])
 
-        return np.sum(self.parameters ** 2) + 2 * dummy
+        return np.sum(
+            [self.parameters[i] * self.parameters[i + np.abs(j)] for j in range(-q, q) for i in range(q - np.abs(j))])
 
     @property
     def variance(self) -> float:
